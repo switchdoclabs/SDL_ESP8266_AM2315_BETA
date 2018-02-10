f4547ab5f86499ccd852b05189a425ce2a8faf89
Project Curacao <projectcuracaopi@gmail.com>
2018-02-10 14:19:46 -0800

    Changed Spike Band to 14.0

--- SDL_ESP8266_HR_AM2315.cpp
+++ SDL_ESP8266_HR_AM2315.cpp
@@ -298,7 +298,7 @@ int SDL_ESP8266_HR_AM2315::internalReadData(float * dataArray)
 
 
 
-#define TEMPERATUREBAND 15.0
+#define TEMPERATUREBAND 14.0
 
       if (lastGoodTemp > (TEMPERATUREBAND + temp)) // too high compared to last good value
       {
